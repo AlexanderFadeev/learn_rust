3a089ac74942deeb89fc8a30ce9bcf028d6357bd
Alexander Fadeev <zander.fadeev@gmail.com>
2022-06-08 01:50:24 +0300

    implement fn fib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn fib(n : u8) -> u64 {
+    if n <= 1 {
+        return 1
+    }
+    fib(n - 1) + fib(n - 2)
+}
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..10 {
+        println!("{}", fib(i));   
+    }
 }
