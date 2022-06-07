f2ac6ef0ba1c6d91733f761787dabaa1487e8087
Alexander Fadeev <zander.fadeev@gmail.com>
2022-06-08 02:36:48 +0300

    Add dynamic fib impl

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use std::time::{Duration, Instant};
 
-const MAX_FIB_INDEX : u8 = 93;
+const MAX_FIB_INDEX : usize = 92;
 
-fn fib_recursive(n : u8) -> u64 {
+fn fib_recursive(n : usize) -> u64 {
     if n <= 1 {
         return 1
     }
     fib_recursive(n - 1) + fib_recursive(n - 2)
 }
 
-fn bench_fib(f: fn(u8) -> u64, impl_name: &str) {
+fn fib_dynamic(n : usize) -> u64 {
+    if n <= 1 {
+        return 1
+    }
+
+    let mut a: [u64; MAX_FIB_INDEX + 1] = [0; MAX_FIB_INDEX + 1];
+    a[0] = 1;
+    a[1] = 1;
+    for i in 2..=n {
+        a[i] = a[i-1] + a[i-2];
+    }
+
+    return a[n];
+}
+
+fn bench_fib(f: fn(usize) -> u64, impl_name: &str) {
     println!("Benchmarking {}", impl_name);
     let start = Instant::now();
-    let mut i = 0_u8;
+    let mut i = 0_usize;
     let mut x : u64 = 0;
     loop {
         if i > MAX_FIB_INDEX {
@@ -33,4 +48,5 @@ fn bench_fib(f: fn(u8) -> u64, impl_name: &str) {
 
 fn main() {
     bench_fib(fib_recursive, "fib_recursive");
+    bench_fib(fib_dynamic, "fib_dynamic");
 }
