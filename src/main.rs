a52f272059022ba42d671e8bfd2da8da8853815f
Alexander Fadeev <zander.fadeev@gmail.com>
2022-06-08 02:14:02 +0300

    Add fib benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-fn fib(n : u8) -> u64 {
+use std::time::{Duration, Instant};
+
+const MAX_FIB_INDEX : u8 = 93;
+
+fn fib_recursive(n : u8) -> u64 {
     if n <= 1 {
         return 1
     }
-    fib(n - 1) + fib(n - 2)
+    fib_recursive(n - 1) + fib_recursive(n - 2)
+}
+
+fn bench_fib(f: fn(u8) -> u64, impl_name: &str) {
+    println!("Benchmarking {}", impl_name);
+    let start = Instant::now();
+    let mut i = 0_u8;
+    let mut x : u64 = 0;
+    loop {
+        if i > MAX_FIB_INDEX {
+            break;
+        }
+        let now = Instant::now();
+        if (now - start) > Duration::from_secs(1) {
+            break;
+        }
+        
+        x = f(i);
+        i += 1;
+    }
+    let finish = Instant::now();
+    let elapsed = finish - start;
+    println!("Elapsed {:?}, {} iterations, {} max value", elapsed, i, x);  
 }
 
 fn main() {
-    for i in 0..10 {
-        println!("{}", fib(i));   
-    }
+    bench_fib(fib_recursive, "fib_recursive");
 }
